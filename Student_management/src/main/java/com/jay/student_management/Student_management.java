464eeac5b7989b7935e31a5ad702e99db1e62ef1
Jayapal28 <131662021+Jayapal28@users.noreply.github.com>
2023-06-24 10:48:45 +0530

    Add files via upload

---
+++ Student_management/src/main/java/com/jay/student_management/Student_management.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.jay.student_management;
+
+/**
+ *
+ * @author JAYAPAL
+ */
+public class Student_management {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
